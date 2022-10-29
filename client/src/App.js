65684d8e08b7e0178f68a67d9edb3b291584e8b2
Sumon Chhetry <sumonchetry1@gmail.com>
2022-10-29 22:58:22 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import { Container, AppBar, Typography, Grow, Grid } from '@mui/material'
+
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
 import memories from './images/memories.png'
+import useStyles from './styles'
 
 const App = () => {
+  const classes = useStyles()
   return (
-    <Container maxWidth='lg'>
-      <AppBar position='static' color='inherit'>
-        <Typography variant='h2' align='center'>
+    <Container maxidth='lg'>
+      <AppBar className={classes.appBar} position='static' color='inherit'>
+        <Typography className={classes.heading} variant='h2' align='center'>
           Complete MERN app
         </Typography>
-        <img src={memories} alt='memories' height='60' />
+        <img
+          className={classes.image}
+          src={memories}
+          alt='memories'
+          height='60'
+        />
       </AppBar>
       <Grow in>
         <Container>
